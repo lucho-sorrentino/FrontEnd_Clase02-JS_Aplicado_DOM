9c4fd633e47365bc963d16d05c2451bed79b7697
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-07-30 03:07:28 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//             ·············· Ejercicios de DOM ··············
+
+
+//     *********Ejercicio 0 *********
+// En base al siguiente HTML, mostrar por consola el texto de los siguientes elementos:
+/* 
+<span>Uno</span>
+<p class="guayerd"> Setenta </p>
+<span class="guayerd"> Cinco </span>
+<span> Dos </span>
+<span> Test </span>
+<span class="guayerd"> Cuatro </span>
+<span id="hola"> Tres </span>
+<p>Diez</p> */
+
+
+// a) El primer span
+// console.log( document.querySelector("span").innerText)
+
+// b) El primer span con clase “guayerd”
+// console.log( document.querySelector(".guayerd").innerText)
+
+// c) El span con id “hola”
+// console.log( document.querySelector("#hola").innerText)
+
+// d) Todos los span
+
+
+// e) Todos los elementos con clase “guayerd”
+
+
+// f) Todos los span con clase “guayerd”.
+
+
+// f) Todos los span y párrafos.
